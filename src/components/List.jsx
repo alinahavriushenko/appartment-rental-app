837ba015b2de4c8b8751b1e8334a7ec73fa5d0d4
Alina Havriushenko <alinagvr@gmail.com>
2024-02-06 20:40:40 +0100

    add card style

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -13,20 +13,28 @@ const List = () => {
   let mappedList = list.map((elem) => {
     return (
       <div key={elem.id} className="listContainer">
-        <p>{elem.country}</p>
-        <p>{elem.city}</p>
-        {elem.neighbourhood && <p>{elem.neighbourhood}</p>}
-        <p>accommodates: {elem.accommodates}</p>
-        <p>{elem.price} $/day</p>
-        <button
-          onClick={() => {
-            deleteBtn(elem.id);
-          }}
-        >
-          delete
-        </button>
         <div className="imgContainer">
           <img src={elem.picture_url.url} alt="productImg" />
+        </div>
+        <div className="cardText">
+          <h3>{elem.name.toUpperCase()}</h3>
+          <div className="rowContainer">
+            <img src="src/assets/img/Location.png" className="icon" />
+            <p>
+              {elem.country}, {elem.city}
+            </p>
+          </div>
+          {/* {elem.neighbourhood && <p>{elem.neighbourhood}</p>} */}
+          <p>accommodates: {elem.accommodates}</p>
+          <p>{elem.price} $ / night</p>
+          <img
+            onClick={() => {
+              deleteBtn(elem.id);
+            }}
+            src="src/assets/img/Delete.png"
+            className="icon delete"
+          />
+          {/* 🗑️ */}
         </div>
       </div>
     );
